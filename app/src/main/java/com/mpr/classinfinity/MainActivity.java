c271d75cf28f28fd4700b5e3ad03546727aa4ebc
Rajkamal <rajkamal.singh1011@gmail.com>
2021-10-05 13:56:44 +0530

    Commit: Designed the whole main page of application ,  added bottom navigation bar and all specific fragments according to the navigation bar item sections.

--- app/src/main/java/com/mpr/classinfinity/MainActivity.java
+++ app/src/main/java/com/mpr/classinfinity/MainActivity.java
@@ -1,14 +1,69 @@
 package com.mpr.classinfinity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+import com.mpr.classinfinity.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    /**
+                     * if we are in Activity then so i used getSupportFragmentManger()
+                     * else if we were in fragment then use getFragmentManager()
+                     */
+                    case R.id.home:
+                        FragmentTransaction homeTransaction = getSupportFragmentManager().beginTransaction();
+                        homeTransaction.replace(R.id.home_fragment,new HomeFragment());
+                        homeTransaction.commit();
+                        break;
+
+                    case R.id.setting_menu:
+                        FragmentTransaction settingsTransaction = getSupportFragmentManager().beginTransaction();
+                        settingsTransaction.replace(R.id.setting_fragment,new SettingsFragment());
+                        settingsTransaction.commit();
+                        break;
+
+                    case R.id.chatBot:
+                        FragmentTransaction chatBotTransaction = getSupportFragmentManager().beginTransaction();
+                        chatBotTransaction.replace(R.id.chatBot_fragment,new ChatBotFragment());
+                        chatBotTransaction.commit();
+                        break;
+
+                    case R.id.scanner:
+                        FragmentTransaction scannerTransaction = getSupportFragmentManager().beginTransaction();
+                        scannerTransaction.replace(R.id.scanner_fragment,new ScannerFragment());
+                        scannerTransaction.commit();
+                        break;
+
+                    case R.id.books:
+                        FragmentTransaction booksTransaction = getSupportFragmentManager().beginTransaction();
+                        booksTransaction.replace(R.id.books_fragment,new BooksFragment());
+                        booksTransaction.commit();
+                        break;
+
+                }
+                return false;
+            }
+        });
+
     }
 }
